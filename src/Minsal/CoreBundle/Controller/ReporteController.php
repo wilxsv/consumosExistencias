bc1b91a5b206f45e9ef00b21a3a875a644c10836
wilxsv <wixl.sv@gmail.com>
2017-08-07 00:43:03 -0600

    core de reportes

---
+++ src/Minsal/CoreBundle/Controller/ReporteController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Minsal\CoreBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Doctrine\ORM\Query\ResultSetMapping;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+class ReporteController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('MinsalCoreBundle:Reporte:index.html.twig' );
+    }
+}
